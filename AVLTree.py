1d3213c533538765bf1df53382e31d8ee94fa2cd
Tetramad <cbwjd777@gmail.com>
2021-11-17 16:52:43 +0900

    AVLTree: implements deletion. maybe it works
    Signed-off-by: Tetramad <cbwjd777@gmail.com>

--- AVLTree.py
+++ AVLTree.py
@@ -2,6 +2,7 @@
 class AVLTreeNode:
     def __init__(self, x):
         self.value = x
+        self.pnode = None
         self.lnode = None
         self.rnode = None
 
@@ -16,11 +17,13 @@ class AVLTreeNode:
                 self.lnode.add(x)
             else:
                 self.lnode = AVLTreeNode(x)
+                self.lnode.pnode = self
         elif self.value < x:
             if self.rnode:
                 self.rnode.add(x)
             else:
                 self.rnode = AVLTreeNode(x)
+                self.rnode.pnode = self
         else:
             pass
 
@@ -35,11 +38,13 @@ class AVLTreeNode:
             if lheight - rheight <= 0:  # RR
                 a, b = self, self.rnode
                 a.rnode, b.lnode = b.lnode, a
+                b.pnode, a.pnode = a.pnode, b
                 return b
             else:  # RL
                 a, b, c = self, self.rnode, self.rnode.lnode
                 b.lnode, c.rnode = c.rnode, b
                 a.rnode, c.lnode = c.lnode, a
+                c.pnode, a.pnode, b.pnode = a.pnode, c, c
                 return c
         else:
             lheight = self.lnode.lnode and self.lnode.lnode.height() or 0
@@ -47,11 +52,13 @@ class AVLTreeNode:
             if lheight - rheight >= 0:  # LL
                 a, b = self, self.lnode
                 a.lnode, b.rnode = b.rnode, a
+                b.pnode, a.pnode = a.pnode, b
                 return b
             else:  # LR
                 a, b, c = self, self.lnode, self.lnode.rnode
                 b.rnode, c.lnode = c.lnode, b
                 a.lnode, c.rnode = c.rnode, a
+                c.pnode, a.pnode, b.pnode = a.pnode, c, c
                 return c
 
     def balanced_add(self, x):
@@ -60,11 +67,13 @@ class AVLTreeNode:
                 self.lnode = self.lnode.balanced_add(x)
             else:
                 self.lnode = AVLTreeNode(x)
+            self.lnode.pnode = self
         elif self.value < x:
             if self.rnode:
                 self.rnode = self.rnode.balanced_add(x)
             else:
                 self.rnode = AVLTreeNode(x)
+            self.rnode.pnode = self
         else:
             pass
         return self.balance()
@@ -80,21 +89,55 @@ class AVLTreeNode:
     def delete(self, x):
         if self.value > x:
             if self.lnode is not None:
-                _, _ = self.lnode.delete(x)
+                self.lnode, result = self.lnode.delete(x)
+                return self, result
             else:
                 return self, False
         elif self.value < x:
             if self.rnode is not None:
-                _, _ = self.rnode.delete(x)
+                self.rnode, result = self.rnode.delete(x)
+                return self, result
             else:
                 return self, False
         else:
             if self.lnode is None and self.rnode is None:
                 return None, True
             elif self.lnode is not None and self.rnode is None:
+                self.lnode.pnode = None
                 return self.lnode, True
             elif self.lnode is None and self.rnode is not None:
+                self.rnode.pnode = None
                 return self.rnode, True
             else:
-                pass
-        return self, False
+                candidate = self.rnode
+                while candidate.lnode:
+                    candidate = candidate.lnode
+                successor = candidate
+                assert successor is not None
+                assert type(successor.pnode) is AVLTreeNode
+                if successor is self.rnode:
+                    successor.lnode = self.lnode
+                    self.lnode.pnode = successor
+                    successor.pnode = self.pnode
+                    if self.pnode:
+                        self.pnode.lnode = successor
+                else:
+                    successor.pnode.lnode = successor.rnode
+                    pnode = successor.pnode
+                    while pnode is not self:
+                        pnode = pnode.balance().pnode
+                    successor.pnode = self.pnode
+                    successor.lnode = self.lnode
+                    if self.lnode:
+                        self.lnode.pnode = successor
+                    successor.rnode = self.rnode
+                    if self.rnode:
+                        self.rnode.pnode = successor
+                return successor, True
+
+    def inorder(self):
+        return (
+            *(self.lnode.inorder() if self.lnode else ()),
+            self.value,
+            *(self.rnode.inorder() if self.rnode else ())
+        )
--- AVLTree_test.py
+++ AVLTree_test.py
@@ -14,20 +14,24 @@ class AVLTreeNodeTest(TestCase):
     def test_creation(self):
         node = AVLTreeNode(42)
         self.assertEqual(node.value, 42)
+        self.assertIsNone(node.pnode)
         self.assertIsNone(node.lnode)
         self.assertIsNone(node.rnode)
 
     def test_add_left(self):
         node = self.build(AVLTreeNode.add, [0, -1])
         self.assertEqual(node.lnode.value, -1)
+        self.assertIs(node.lnode.pnode, node)
 
     def test_add_right(self):
         node = self.build(AVLTreeNode.add, [0, 1])
         self.assertEqual(node.rnode.value, 1)
+        self.assertIs(node.rnode.pnode, node)
 
     def test_same_value(self):
         node = self.build(AVLTreeNode.add, [0, 0])
         self.assertEqual(node.value, 0)
+        self.assertIsNone(node.pnode)
         self.assertIsNone(node.lnode)
         self.assertIsNone(node.rnode)
 
@@ -37,6 +41,9 @@ class AVLTreeNodeTest(TestCase):
         self.assertEqual(node.value, -1)
         self.assertEqual(node.lnode.value, -2)
         self.assertEqual(node.rnode.value, 0)
+        self.assertIsNone(node.pnode)
+        self.assertIs(node.lnode.pnode, node)
+        self.assertIs(node.rnode.pnode, node)
 
     def test_add_rightright(self):
         node = self.build(AVLTreeNode.add, [0, 1, 2])
@@ -44,6 +51,9 @@ class AVLTreeNodeTest(TestCase):
         self.assertEqual(node.value, 1)
         self.assertEqual(node.lnode.value, 0)
         self.assertEqual(node.rnode.value, 2)
+        self.assertIsNone(node.pnode)
+        self.assertIs(node.lnode.pnode, node)
+        self.assertIs(node.rnode.pnode, node)
 
     def test_add_leftright(self):
         node = self.build(AVLTreeNode.add, [0, -2, -1])
@@ -51,6 +61,9 @@ class AVLTreeNodeTest(TestCase):
         self.assertEqual(node.value, -1)
         self.assertEqual(node.lnode.value, -2)
         self.assertEqual(node.rnode.value, 0)
+        self.assertIsNone(node.pnode)
+        self.assertIs(node.lnode.pnode, node)
+        self.assertIs(node.rnode.pnode, node)
 
     def test_add_rightleft(self):
         node = self.build(AVLTreeNode.add, [0, 2, 1])
@@ -58,24 +71,39 @@ class AVLTreeNodeTest(TestCase):
         self.assertEqual(node.value, 1)
         self.assertEqual(node.lnode.value, 0)
         self.assertEqual(node.rnode.value, 2)
+        self.assertIsNone(node.pnode)
+        self.assertIs(node.lnode.pnode, node)
+        self.assertIs(node.rnode.pnode, node)
 
     def test_balanced_add(self):
         node = self.build(AVLTreeNode.balanced_add, [0, -1, -2])
         self.assertEqual(node.value, -1)
+        self.assertIsNone(node.pnode)
         self.assertEqual(node.lnode.value, -2)
+        self.assertIs(node.lnode.pnode, node)
         self.assertEqual(node.rnode.value, 0)
+        self.assertIs(node.rnode.pnode, node)
         node = self.build(AVLTreeNode.balanced_add, [0, 1, 2])
         self.assertEqual(node.value, 1)
+        self.assertIsNone(node.pnode)
         self.assertEqual(node.lnode.value, 0)
+        self.assertIs(node.lnode.pnode, node)
         self.assertEqual(node.rnode.value, 2)
+        self.assertIs(node.rnode.pnode, node)
         node = self.build(AVLTreeNode.balanced_add, [0, -2, -1])
         self.assertEqual(node.value, -1)
+        self.assertIsNone(node.pnode)
         self.assertEqual(node.lnode.value, -2)
+        self.assertIs(node.lnode.pnode, node)
         self.assertEqual(node.rnode.value, 0)
+        self.assertIs(node.rnode.pnode, node)
         node = self.build(AVLTreeNode.balanced_add, [0, 1, 2])
         self.assertEqual(node.value, 1)
+        self.assertIsNone(node.pnode)
         self.assertEqual(node.lnode.value, 0)
+        self.assertIs(node.lnode.pnode, node)
         self.assertEqual(node.rnode.value, 2)
+        self.assertIs(node.rnode.pnode, node)
         node = self.build(AVLTreeNode.balanced_add, [0, 1, 2, 3, 4])
         self.assertEqual(node.value, 1)
         self.assertEqual(node.lnode.value, 0)
@@ -99,7 +127,6 @@ class AVLTreeNodeTest(TestCase):
         self.assertEqual(new_node, node)
         self.assertFalse(result)
 
-    @skip('BinarySearchTree도 못 만들면서 뭘 만들겠다고...')
     def test_delete_exists(self):
         node = self.build(AVLTreeNode.balanced_add, [0])
         new_node, result = node.delete(0)
@@ -119,5 +146,17 @@ class AVLTreeNodeTest(TestCase):
         node = self.build(AVLTreeNode.balanced_add, [0, -1, 1])
         new_node, result = node.delete(0)
         self.assertIsNotNone(new_node)
-        self.assertNotEqual(new_node, node)
         self.assertTrue(result)
+        self.assertIn(new_node.value, (-1, 1))
+        self.assertIs((new_node.lnode or new_node.rnode).pnode, new_node)
+        node = self.build(AVLTreeNode.balanced_add, range(-3, 4))
+        deletes = (1, 3, -2, -1, 2, 0, -3)
+        for i, v in enumerate(deletes):
+            node, result = node.delete(v)
+            self.assertTrue(result)
+            for x in deletes[i + 1:]:
+                self.assertTrue(node.find(x))
+
+    def test_inorder(self):
+        node = self.build(AVLTreeNode.balanced_add, [0, -2, -3, -1, 2, 1, 3])
+        self.assertEqual(node.inorder(), (-3, -2, -1, 0, 1, 2, 3))
